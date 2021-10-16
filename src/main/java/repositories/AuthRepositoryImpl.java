8edd3d29a653e5e7b2c87891cb2358008e82afa3
Gordeev.Ilya <gordeevilyas@yandex.ru>
2021-10-16 16:17:30 +0300

    Add registration and authorisation

---
+++ src/main/java/repositories/AuthRepositoryImpl.java
@@ -0,0 +1,41 @@
+package repositories;
+
+import models.Auth;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+
+public class AuthRepositoryImpl implements AuthRepository{
+
+    private Connection connection;
+
+    public AuthRepositoryImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public Auth findByCookieValue(String cookieValue) {
+        return null;
+    }
+
+    @Override
+    public List<Auth> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Auth> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Auth save(Auth auth) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
